10ee43ef5f1e94a81daf7da072441336f3385b79
Berbet16 <63414835+Berbet16@users.noreply.github.com>
2021-04-08 19:10:41 +0300

    Add project files.

---
+++ WebApplication3/Controllers/DefaultController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("hakkimizda")]
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        [Route("urunler")]
+        public ActionResult Urunler()
+        {
+            return View();
+        }
+        [Route("magaza")]
+        public ActionResult Magaza()
+        {
+            return View();
+        }
+        [Route("iletisim")]
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
